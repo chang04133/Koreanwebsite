d2f1caeb7d81f6badfba83daa7727638986c5b9b
Chang <changy.maeng@gmail.com>
2017-03-05 16:10:48 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 import { AppComponent }  from './app.component';
+import { SidebarComponent } from './sidebar.component';
+import { HeaderComponent } from './header.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, CommonModule],
+  declarations: [ AppComponent, SidebarComponent, HeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
